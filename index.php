75625ef21c5deb0c0a5e14ae2a254a5e2ddd59f5
Scrirock <73163680+Scrirock@users.noreply.github.com>
2021-02-03 14:26:41 +0100

    exo fini

--- index.php
+++ index.php
@@ -2,10 +2,20 @@
 
 // Premiere ligne
 
+    session_start();
+    $_SESSION["couleur"] = "rouge"
+?>
 
-
-
-
-
-
-
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>rouge</title>
+</head>
+<body>
+<a href="index2.php">aller a index2.php</a>
+</body>
+</html>
